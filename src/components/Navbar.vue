55c486ebae91a83c28a9683760220f0ef8a9d5f5
htg731115 <382891990@qq.com>
2019-03-03 23:12:05 +0800

    2

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,21 +2,23 @@
     <div>
         <el-row>
             <el-col :span="4"  >
-                
+                1
             </el-col>
-            <el-col :span="5" push="20">
-                <img src="../assets/full.svg" width="30px" @click="click()"/>
-
- 
+            <el-col :span="5" :offset="10">
+                <img src="../assets/full.svg" class="full-img" @click="click()"/>
+                <img src="../assets/userlog.svg" class="full-img" width="100%">
             </el-col>
         </el-row>
     </div>
 </template>
 <script>
+import screenfull from 'screenfull'
 export default {
     data(){
         return{
-
+            click(){
+                screenfull.toggle();
+            }
         }
     },
     methods:{
@@ -28,5 +30,11 @@ export default {
 </script>
 
 <style lang="scss">
-
+.full-img{
+    width:30px;
+    margin-top:10px;
+    padding: 10px 10px 10px 10px; 
+    cursor: pointer;
+    
+}
 </style>
